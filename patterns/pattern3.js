db46bbbcd875bbda5deef752cf626427c172db3f
Chandru K <98522358+CHANDRUK29@users.noreply.github.com>
2024-08-07 17:18:54 +0530

    new pattern added

---
+++ patterns/pattern3.js
@@ -0,0 +1,31 @@
+/*
+pattern
+Input:
+  Rows: 5
+Output:
+
+       *
+
+     * * *
+
+    * * * * *
+
+  * * * * * * *
+
+* * * * * * * * *
+
+*/
+function printPattern(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let str = '';
+    for (let j = 1; j <= rows - i; j++) {
+      str += '  ';
+    }
+    for (let k = 1; k <= (2 * i - 1); k++) {
+      str += '* ';
+    }
+
+    console.log(str);
+  }
+}
+printPattern(5)
