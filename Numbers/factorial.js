02b7ea197c92e3c3558eccfaff55d51f2d643add
Chandru K <98522358+CHANDRUK29@users.noreply.github.com>
2024-08-09 13:20:20 +0530

    factorial

---
+++ Numbers/factorial.js
@@ -0,0 +1,17 @@
+// logic --> 
+// 0+1 =1
+// 1+1 =2
+// 1+2 =3
+// 2+3 =5
+
+
+// Big-O = O(n)
+function factorial(n) {
+    let val = 1
+    for (let i = n; i > 0; i--) {
+        val = val * i
+    }
+    return val
+}
+
+console.log(factorial(5))
