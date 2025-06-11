942e58545969d3f097fb14c7c1230dcf6f369d4a
chandru <chandruviru143@gmail.com>
2025-06-11 13:56:43 +0530

    added custom reduce function

--- problems.js
+++ problems.js
@@ -312,3 +312,26 @@ const original = {
 
 const copy = deepCopy(original);
 console.log(copy);
+
+
+// custom Reduce Function 
+Array.prototype.myReduce = function (callback, initialValue) {
+  // check if array is empty
+  if (this.length === 0 && initialValue === undefined) {
+    throw new TypeError('Empty Array with no intial value')
+  }
+  let accumulator = initialValue !== undefined ? initialValue : this[0];
+  let startIndex = initialValue !== undefined ? 0 : 1;
+
+  for (let i = startIndex; i < this.length; i++) {
+    if (i in this) { // condition to check array doesnot have empty value since it doesnot able to execute reduce function
+      accumulator = callback(accumulator, this[i], i, this)
+    }
+  }
+  return accumulator;
+}
+const num = [1, 2, 3, 4]
+const sum = num.myReduce((acc, curval) => acc + curval, 0);
+const product = num.myReduce((acc, curval) => acc * curval, 1);
+console.log(sum)
+console.log(product)
