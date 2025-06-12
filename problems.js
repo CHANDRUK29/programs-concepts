d5a05d9eb4ff3c4c49ea07cdecebf7584b972a05
chandru <chandruviru143@gmail.com>
2025-06-12 17:38:49 +0530

    added custom map function

--- problems.js
+++ problems.js
@@ -335,3 +335,26 @@ const sum = num.myReduce((acc, curval) => acc + curval, 0);
 const product = num.myReduce((acc, curval) => acc * curval, 1);
 console.log(sum)
 console.log(product)
+
+
+//custom map function
+Array.prototype.myMap = function (callback, thisArg) {
+  //check callback is function
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + 'is not a function');
+  }
+  const result = []
+  for (let i = 0; i < this.length; i++) {
+    if (i in this) {
+      result[i] = callback.call(thisArg, this[i], i, this)
+    }
+  }
+  return result;
+}
+const nums = [1, 2, 3];
+
+const squared = nums.myMap((num, idx, arr) => {
+  return num * num;
+});
+
+console.log(squared); // [1, 4, 9]
