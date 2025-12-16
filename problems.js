dc1ac19fd852327ed64035b46509b32e0b65ad19
chandru <chandruviru143@gmail.com>
2025-12-16 19:56:56 +0530

    added new problems

--- problems.js
+++ problems.js
@@ -358,3 +358,19 @@ const squared = nums.myMap((num, idx, arr) => {
 });
 
 console.log(squared); // [1, 4, 9]
+
+// merging string Alternatively
+function mergeStrAlternatively(str1, str2) {
+  let result = ''
+  let maxLen = Math.max(str1.length, str2.length)
+  for (let i = 0; i < maxLen; i++) {
+    if (i < str1.length) {
+      result += str1[i]
+    }
+    if (i < str2.length) {
+      result += str2[i]
+    }
+  }
+  return result
+}
+console.log(mergeStrAlternatively("chandru", "viru"))//cvhiarnudru
