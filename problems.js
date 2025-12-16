338a270e402ad6ba02a987f4f6b94da14452b5d2
chandru <chandruviru143@gmail.com>
2025-12-16 20:05:54 +0530

    added new problems

--- problems.js
+++ problems.js
@@ -374,3 +374,19 @@ function mergeStrAlternatively(str1, str2) {
   return result
 }
 console.log(mergeStrAlternatively("chandru", "viru"))//cvhiarnudru
+
+//reversing the words
+var reverseWords = function (s) {
+  let res = '';
+  let word = '';
+  for (let c of s) {
+    if (c === ' ') {
+      res += word + c;
+      word = '';
+    } else {
+      word = c + word;
+    }
+  }
+  return res + word;
+};
+console.log(reverseWords("priya bagde"))
